5bb68a97d4f1efb856cc3a837554357d4a28759c
Joe Foray <you@example.com>
2025-10-18 19:31:01 -0400

    changed boat model

--- boats_and_locations/models.py
+++ boats_and_locations/models.py
@@ -21,27 +21,12 @@ class Marina(models.Model):
 class Boat(models.Model):
     name = models.CharField(max_length=100)
     boat_type = models.CharField(max_length=50)
+    length = models.CharField(max_length=2)
+    passengers = models.CharField(max_length=2)
+    max_hp = models.CharField(max_length=4)
     description = models.TextField()
-    marina = models.ForeignKey(Marina, on_delete=models.CASCADE, related_name='boats')
-    open = models.BooleanField(default=True)
-    available_to_customers = models.BooleanField(default=True)
-    issues = models.TextField() # ex. depth finder not working
-    rules = models.TextField()  #ex. no pets allowed
-    image = models.ImageField(default='..static/images/dbc-logo-small.png')
+    main_image = models.ImageField(default='..static/images/dbc-logo-small.png')
+    images = models.ImageField(default='..static/images/')
 
     def __str__(self):
-        return f"{self.name} ({self.marina.name})"
-    
-    def is_open(self):
-        
-        return self.open
-    
-    def booked(self,date,time_slot):
-        self.open = False
-        self.available_to_customers = False
-        self.save()
-
-    def reset_availability(self):
-        self.open = True
-        self.available_to_customers = True
-        self.save()
+        return f"{self.name}"
