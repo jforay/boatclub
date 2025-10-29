76c6d16858121bc56555deab78da7e7a66c8436e
Joe Foray <you@example.com>
2025-10-29 16:57:11 -0400

    filling databsae

--- boatclub/settings.py
+++ boatclub/settings.py
@@ -83,9 +83,14 @@ WSGI_APPLICATION = 'boatclub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
