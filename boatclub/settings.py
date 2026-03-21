5cbb7f347f18a30727a9f73015dd0369be8a9b9e
Joe Foray <you@example.com>
2026-03-21 18:54:16 -0400

    fixed s3 ACL

--- boatclub/settings.py
+++ boatclub/settings.py
@@ -185,7 +185,7 @@ if os.environ.get('AWS_ACCESS_KEY_ID'):
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
