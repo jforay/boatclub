fa3a9772ee84faff441677077faccd2e0bb8a231
Joe Foray <you@example.com>
2026-03-25 17:34:59 -0400

    fixed locations url

--- boatclub/urls.py
+++ boatclub/urls.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from boats_and_locations import views as bl_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('home.urls')),
-    
+
     path('fleet/',include('boats_and_locations.urls')),
     path('users/', include('users.urls')),
 
-
+    path('<slug:slug>/', bl_views.marina_flyer_view, name='marina_flyer_root'),
 
 ]
 
